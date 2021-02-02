ebfc376e6e753c467295b6a59d9ffddf2c0d2e53
alisonhussey <alisonhussey@gmail.com>
2021-02-02 03:09:34 +0000

    refactored Question class to generate 1 random question, refactored Game class to include more of required lines

--- GameLoop.rb
+++ GameLoop.rb
@@ -1,8 +1,8 @@
 class Game
-  def initialize(p1, p2, qs)
+  def initialize(p1, p2, q)
     @player_1 = p1
     @player_2 = p2
-    @questions = qs
+    @question = q
     @current_player = @player_1
     @current_question_index = 0
   end
@@ -12,34 +12,41 @@ class Game
     while @player_1.lives > 0 && @player_2.lives > 0
       ask_question()
     end
+
   end
 
-  def update_question_index
-    if @current_question_index < @questions.length - 1
-      @current_question_index += 1
-    else
-      @current_question_index = 0
-    end
-    puts @current_question_index
-    puts @questions.length
-  end
+  # def update_question_index
+  #   if @current_question_index < @questions.length - 1
+  #     @current_question_index += 1
+  #   else
+  #     @current_question_index = 0
+  #   end
+  #   puts @current_question_index
+  #   puts @questions.length
+  # end
     
-  def generate_question
-    takes a random number
-  end
+
 
   def ask_question()
-    puts "#{@current_player.name}: #{@questions[@current_question_index].question}"
+    @new_question = Question.new
+    puts "#{@current_player.name}: #{@new_question.question}"
     print "> "
-    choice = $stdin.gets.chomp
-    if choice == @questions[@current_question_index].answer
-      puts "You win"
+    choice = $stdin.gets.chomp.to_i
+    if choice == @new_question.answer
+      puts "Yes! You are correct."
     else
       @current_player.lives -= 1
-      puts "You lose, your lives: #{@current_player.lives}"
+      puts "Seriously? No!"
     end 
-    update_player()
-    update_question_index()
+    puts "P1: #{@player_1.lives}/3 vs P2: #{@player_2.lives}/3"
+    if @player_1.lives == 0 || @player_2.lives == 0
+      puts "----- GAME OVER -----"
+      puts "Good bye!"
+    else 
+      puts "----- NEW TURN -----"
+      update_player()
+    end
+    # update_question_index()
   end
 
   def update_player
@@ -60,26 +67,30 @@ class Player
   end
 end
 
+
 class Question
   attr_accessor :question, :answer
-  def initialize(q, a)
-    @question = q
-    @answer = a
-
+  def initialize
+    @random_1 = rand(1..10)
+    @random_2 = rand(1..10)
+    @question = "What does #{@random_1} plus #{@random_2} equal?"
+    @answer = @random_1 + @random_2
   end
 end
 
 player_1 = Player.new("Player 1")
 player_2 = Player.new("Player 2")
-question_1 = Question.new("What does 5 plus 3 equal?", "8")
-question_2 = Question.new("What does 2 plus 4 equal?", "6")
-all_questions = []
-all_questions << question_1 
-all_questions << question_2
+# question_1 = Question.new("What does 5 plus 3 equal?", "8")
+# question_2 = Question.new("What does 2 plus 4 equal?", "6")
+# all_questions = []
+# all_questions << question_1 
+# all_questions << question_2
 # print all_questions
-game1 = Game.new(player_1, player_2, all_questions)
+q = Question.new
+game1 = Game.new(player_1, player_2, q)
 # puts game1.inspect
 
 # game1.ask_question
 
-game1.start
+ game1.start
+
---
+++ main.rb
@@ -0,0 +1,2 @@
+require "./GameLoop.rb"
+
